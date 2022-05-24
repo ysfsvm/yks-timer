52cc92d6199569e94b4a78e437cf4eb1f4f9019e
patwork <patwork@gmail.com>
2022-05-24 17:31:18 +0200

    icon and label

--- extension.js
+++ extension.js
@@ -17,18 +17,61 @@
  */
 
 /* exported init */
+"use strict";
+
+const St = imports.gi.St;
+const Gio = imports.gi.Gio;
+const Clutter = imports.gi.Clutter;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 
 class Extension {
     constructor() {
+        this._indicator = null;
     }
 
     enable() {
+        log(`enabling ${Me.metadata.name}`);
+
+        let indicatorName = `${Me.metadata.name} Indicator`;
+
+        // Create a panel button
+        this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+
+        // Add an icon
+        let icon = new St.Icon({
+            gicon: new Gio.ThemedIcon({ name: "face-laugh-symbolic" }),
+            style_class: "system-status-icon",
+        });
+
+        // Add label
+        let label = new St.Label({ text: "ho ho ho", style_class: 'ns-horizontal-label', y_align: Clutter.ActorAlign.CENTER});
+
+        // Create box
+        let box = new St.BoxLayout();
+        box.add_child(icon);
+        box.add_child(label);
+        this._indicator.add_child(box);
+
+        // `Main.panel` is the actual panel you see at the top of the screen,
+        // not a class constructor.
+        Main.panel.addToStatusArea(indicatorName, this._indicator);
     }
 
     disable() {
+        log(`disabling ${Me.metadata.name}`);
+
+        this._indicator.destroy();
+        this._indicator = null;
     }
 }
 
 function init() {
+    log(`initializing ${Me.metadata.name}`);
     return new Extension();
 }
+
+/* EoF */
